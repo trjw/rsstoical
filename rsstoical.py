4d604b50b1964874bd31cdc4a3c3d6f2f1c78103
John Williams <j.williams1@uq.edu.au>
2022-02-02 11:17:34 +1000

    Initial import of WIP UQ RSS to iCal converter
    Signed-off-by: John Williams <j.williams1@uq.edu.au>

---
+++ rsstoical.py
@@ -0,0 +1,52 @@
+#/usr/bin/python3
+
+from icalendar import Calendar, Event
+import feedparser
+import datetime
+import pytz
+
+url="https://uq.edu.au/events/rss/event_all_feed.php?cid=16"
+
+feed=feedparser.parse(url)
+
+def display(cal):
+    return cal.to_ical().decode().replace('\r\n', '\n').strip()
+
+def shorten_description(text):
+    abbrev={"Examination":"Exam", "Semester":"Sem", "Quarter":"Qtr", "Research":"Rsch"}
+    tmp=text
+    for a in abbrev:
+        tmp=tmp.replace(a,abbrev[a])
+    return tmp
+
+def rsstoical(rss, uid, cal):
+    title=shorten_description(rss['title'])
+
+    summary=shorten_description(rss['summary'])
+
+    date=rss['published_parsed']
+    start=datetime.date(date.tm_year,date.tm_mon, 
+        date.tm_mday)
+    #end=start+timedelta(days=1)
+    event=Event()
+    event.add('summary',title)
+    event.add('description', summary)
+    event.add('uid',"%i" % uid)
+    event.add('dtstart', start)
+    #event.add('dtend', end)
+    #event.add('dtstamp', start)
+
+    cal.add_component(event)
+
+cal=Calendar()
+cal.add('prodid', '-//My Calendar product//mxm.dk//')
+cal.add('version', '2.0')
+
+uid=1
+for entry in feed.entries:
+    rsstoical(entry, uid, cal)
+    uid+=1
+
+print(cal.to_ical().decode())
+
+
